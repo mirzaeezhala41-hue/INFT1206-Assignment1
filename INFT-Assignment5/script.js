f7471eb3ca8d56ba690e193347fc88d04f7e72a2
mirzaeezhala41-hue <mirzaeezhala41@gmail.com>
2026-04-19 16:29:10 -0400

    update script.js

--- INFT-Assignment5/script.js
+++ INFT-Assignment5/script.js
@@ -4,23 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const toggleBtn = document.getElementById("toggleBtn");
   const comments = document.getElementById("comments");
 
-  // Hide comments at start
-  comments.style.display = "none";
-  toggleBtn.setAttribute("aria-expanded", "false");
-
-  function toggleComments() {
-    const isHidden = comments.style.display === "none";
-
-    if (isHidden) {
-      comments.style.display = "block";
-      toggleBtn.textContent = "Hide Comments";
-      toggleBtn.setAttribute("aria-expanded", "true");
-    } else {
-      comments.style.display = "none";
-      toggleBtn.textContent = "Show Comments";
-      toggleBtn.setAttribute("aria-expanded", "false");
-    }
-  }
+  
 
   // Mouse click
   toggleBtn.addEventListener("click", toggleComments);
